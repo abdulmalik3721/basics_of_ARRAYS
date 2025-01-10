b881f5246fad972b75adf778368adfc622c3ab9b
ABDUL MALIK <uneeba@gmail.com>
2025-01-10 19:34:03 +0530

    basics of ARRAYS

---
+++ basics_Of_ARRAYS/Main.java
@@ -0,0 +1,19 @@
+package DSA.basics_Of_ARRAYS;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] intArray;
+		intArray = new int[4];
+		intArray[0] = 22;
+		intArray[1] = 44;
+		intArray[2] = 88;
+		intArray[3] = 77;
+		
+		System.out.println(Arrays.toString(intArray));
+
+	}
+
+}
